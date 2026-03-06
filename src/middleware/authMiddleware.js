42b5bc4075b659f344aab824ae96f91d2726c935
Efia <aivilefia@gmail.com>
2026-03-05 20:04:19 -0800

    Add controllers folder

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 
+export const authMiddleware = (req, res, next) => {
 
-// 1. Extract token from Authorization header
-// 2. Verify token
-// 3. Find user
-// 4. Attach user to req.user
-// 5. Call next()
-// 6. If invalid → return 401
+  const authHeader = req.headers.authorization;
 
-const authMiddleware = async (req, res, next) => {
-  //  implement here
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
-
-export default authMiddleware;
